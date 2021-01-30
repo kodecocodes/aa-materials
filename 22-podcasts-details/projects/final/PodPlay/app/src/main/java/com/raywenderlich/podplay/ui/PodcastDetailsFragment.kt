68d26106f7907dcb57aebba6f8681c308d31ba42
Kevin Moore <kevindavmoore@gmail.com>
2021-01-30 14:10:18 -0800

    1-30-21

--- 22-podcasts-details/projects/final/PodPlay/app/src/main/java/com/raywenderlich/podplay/ui/PodcastDetailsFragment.kt
+++ 22-podcasts-details/projects/final/PodPlay/app/src/main/java/com/raywenderlich/podplay/ui/PodcastDetailsFragment.kt
@@ -36,12 +36,15 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import com.bumptech.glide.Glide
 import com.raywenderlich.podplay.R
+import com.raywenderlich.podplay.databinding.ActivityPodcastBinding
+import com.raywenderlich.podplay.databinding.FragmentPodcastDetailsBinding
 import com.raywenderlich.podplay.viewmodel.PodcastViewModel
-import kotlinx.android.synthetic.main.fragment_podcast_details.*
 
 class PodcastDetailsFragment : Fragment() {
 
   private val podcastViewModel: PodcastViewModel by activityViewModels()
+  private lateinit var databinding: FragmentPodcastDetailsBinding
+
 
   companion object {
     fun newInstance(): PodcastDetailsFragment {
@@ -55,8 +58,9 @@ class PodcastDetailsFragment : Fragment() {
   }
 
   override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                            savedInstanceState: Bundle?): View? {
-    return inflater.inflate(R.layout.fragment_podcast_details, container, false)
+                            savedInstanceState: Bundle?): View {
+    databinding = FragmentPodcastDetailsBinding.inflate(inflater, container, false)
+    return databinding.root
   }
 
   override fun onActivityCreated(savedInstanceState: Bundle?) {
@@ -71,10 +75,10 @@ class PodcastDetailsFragment : Fragment() {
 
   private fun updateControls() {
     val viewData = podcastViewModel.activePodcastViewData ?: return
-    feedTitleTextView.text = viewData.feedTitle
-    feedDescTextView.text = viewData.feedDesc
+    databinding.feedTitleTextView.text = viewData.feedTitle
+    databinding.feedDescTextView.text = viewData.feedDesc
     activity?.let { activity ->
-      Glide.with(activity).load(viewData.imageUrl).into(feedImageView)
+      Glide.with(activity).load(viewData.imageUrl).into(databinding.feedImageView)
     }
   }
 }
