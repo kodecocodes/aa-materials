40c6ba2540222e21f35751e12060fb5877edf729
Fuad Kamal <fuad@anaara.com>
2021-01-31 19:06:26 -0500

    clean up
    removal of unused imports

--- 23-podcast-episodes/projects/starter/PodPlay/app/src/main/java/com/raywenderlich/podplay/viewmodel/PodcastViewModel.kt
+++ 23-podcast-episodes/projects/starter/PodPlay/app/src/main/java/com/raywenderlich/podplay/viewmodel/PodcastViewModel.kt
@@ -38,61 +38,42 @@ import com.raywenderlich.podplay.repository.PodcastRepo
 import com.raywenderlich.podplay.viewmodel.SearchViewModel.PodcastSummaryViewData
 import java.util.*
 
-class PodcastViewModel(application: Application) : AndroidViewModel
-(application) {
+class PodcastViewModel(application: Application) : AndroidViewModel(application) {
 
   var podcastRepo: PodcastRepo? = null
   var activePodcastViewData: PodcastViewData? = null
 
-  fun getPodcast(podcastSummaryViewData: PodcastSummaryViewData, callback: (PodcastViewData?) ->
-  Unit) {
+  fun getPodcast(podcastSummaryViewData: PodcastSummaryViewData): PodcastViewData? {
 
-    val repo = podcastRepo ?: return
-    val feedUrl = podcastSummaryViewData.feedUrl ?: return
+    val repo = podcastRepo ?: return null
+    val feedUrl = podcastSummaryViewData.feedUrl ?: return null
 
-    repo.getPodcast(feedUrl) {
-      it?.let {
-        it.feedTitle = podcastSummaryViewData.name ?: ""
-        it.imageUrl = podcastSummaryViewData.imageUrl ?: ""
-        activePodcastViewData = podcastToPodcastView(it)
-        callback(activePodcastViewData)
-      }
+    val podcast = repo.getPodcast(feedUrl)
+    podcast?.let {
+      it.feedTitle = podcastSummaryViewData.name ?: ""
+      it.imageUrl = podcastSummaryViewData.imageUrl ?: ""
+      activePodcastViewData = podcastToPodcastView(it)
+      return activePodcastViewData
     }
+    return null
   }
 
   private fun podcastToPodcastView(podcast: Podcast): PodcastViewData {
-    return PodcastViewData(
-        false,
-        podcast.feedTitle,
-        podcast.feedUrl,
-        podcast.feedDesc,
-        podcast.imageUrl,
-        episodesToEpisodesView(podcast.episodes)
-    )
+    return PodcastViewData(false, podcast.feedTitle, podcast.feedUrl, podcast.feedDesc,
+        podcast.imageUrl, episodesToEpisodesView(podcast.episodes))
   }
 
   private fun episodesToEpisodesView(episodes: List<Episode>): List<EpisodeViewData> {
     return episodes.map {
-      EpisodeViewData(it.guid, it.title, it.description, it.mediaUrl,
-          it.releaseDate, it.duration)
+      EpisodeViewData(it.guid, it.title, it.description, it.mediaUrl, it.releaseDate, it.duration)
     }
   }
 
-  data class PodcastViewData(
-      var subscribed: Boolean = false,
-      var feedTitle: String? = "",
-      var feedUrl: String? = "",
-      var feedDesc: String? = "",
-      var imageUrl: String? = "",
-      var episodes: List<EpisodeViewData>
-  )
+  data class PodcastViewData(var subscribed: Boolean = false, var feedTitle: String? = "",
+                             var feedUrl: String? = "", var feedDesc: String? = "",
+                             var imageUrl: String? = "", var episodes: List<EpisodeViewData>)
 
-  data class EpisodeViewData (
-      var guid: String? = "",
-      var title: String? = "",
-      var description: String? = "",
-      var mediaUrl: String? = "",
-      var releaseDate: Date? = null,
-      var duration: String? = ""
-  )
+  data class EpisodeViewData(var guid: String? = "", var title: String? = "",
+                             var description: String? = "", var mediaUrl: String? = "",
+                             var releaseDate: Date? = null, var duration: String? = "")
 }
