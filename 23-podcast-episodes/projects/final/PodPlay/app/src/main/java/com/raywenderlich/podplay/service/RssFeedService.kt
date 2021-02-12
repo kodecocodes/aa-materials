46e759677e1db5979bb593e4798e54e79d56151f
Fuad Kamal <fuad@anaara.com>
2021-02-11 22:56:17 -0500

    proper retrofit syntax
    needed `@Url` annotation

--- 23-podcast-episodes/projects/final/PodPlay/app/src/main/java/com/raywenderlich/podplay/service/RssFeedService.kt
+++ 23-podcast-episodes/projects/final/PodPlay/app/src/main/java/com/raywenderlich/podplay/service/RssFeedService.kt
@@ -1,51 +1,57 @@
 package com.raywenderlich.podplay.service
 
-import okhttp3.*
-import java.io.IOException
+import com.raywenderlich.podplay.BuildConfig
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.http.GET
+import retrofit2.http.Url
+import java.util.concurrent.TimeUnit
 
-class RssFeedService : FeedService {
-  override suspend fun getFeed(xmlFileURL: String) {
-    // 1
-    val client = OkHttpClient()
-// 2
-    val request = Request.Builder()
-        .url(xmlFileURL)
+class RssFeedService {
+
+  suspend fun getFeed(xmlFileURL: String): Boolean {
+    val service: FeedService
+
+    val interceptor = HttpLoggingInterceptor()
+    interceptor.level = HttpLoggingInterceptor.Level.BODY
+
+    val client = OkHttpClient().newBuilder()
+        .connectTimeout(30, TimeUnit.SECONDS)
+        .writeTimeout(30, TimeUnit.SECONDS)
+        .readTimeout(30, TimeUnit.SECONDS)
+
+    if (BuildConfig.DEBUG) {
+      client.addInterceptor(interceptor)
+    }
+    client.build()
+
+    val retrofit = Retrofit.Builder()
+        .baseUrl("$xmlFileURL/")
         .build()
-// 3
-    client.newCall(request).enqueue(object : Callback {
-      // 4
-      override fun onFailure(call: Call, e: IOException) {
+    service = retrofit.create(FeedService::class.java)
 
+    try {
+      val result = service.getFeed(xmlFileURL)
+      if (result.code() >= 400) {
+        // TODO : // create an error from error body and return
+        println("server error, ${result.code()}, ${result.errorBody()}")
+        return false
+      } else {
+        // return success result
+        println(result.body().toString())
+        return true
       }
-
-      // 5
-      @Throws(IOException::class)
-      override fun onResponse(call: Call, response: Response) {
-// 6
-        if (response.isSuccessful) {
-          // 7
-          response.body()?.let { responseBody ->
-// 8
-            println(responseBody.string())
-            // Parse response and send to callback
-            return
-          }
-        }
-// 9
-
-      }
-    })
+    } catch (t: Throwable) {
+      // TODO : create an error from throwable and return it
+      println("error, ${t.localizedMessage}")
+    }
+    return false
   }
 }
 
 interface FeedService {
-  // 1
-  suspend fun getFeed(xmlFileURL: String)
-
-  // 2
-  companion object {
-    val instance: FeedService by lazy {
-      RssFeedService()
-    }
-  }
+  @GET
+  suspend fun getFeed(@Url xmlFileURL: String): Response<Unit>
 }
