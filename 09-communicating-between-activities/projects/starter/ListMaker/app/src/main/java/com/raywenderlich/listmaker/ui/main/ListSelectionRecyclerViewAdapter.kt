3f165b9115ec8c16137fc28f9d62c8dcc4f5fdd5
Darryl Bayliss <darrylbayliss@dazindustries.co.uk>
2021-02-15 20:53:22 +0000

    Update starter project.

---
+++ 09-communicating-between-activities/projects/starter/ListMaker/app/src/main/java/com/raywenderlich/listmaker/ui/main/ListSelectionRecyclerViewAdapter.kt
@@ -0,0 +1,28 @@
+package com.raywenderlich.listmaker.ui.main
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.raywenderlich.listmaker.databinding.ListSelectionViewHolderBinding
+import com.raywenderlich.listmaker.models.TaskList
+
+class ListSelectionRecyclerViewAdapter(private val lists : MutableList<TaskList>) : RecyclerView.Adapter<ListSelectionViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ListSelectionViewHolder {
+        val binding = ListSelectionViewHolderBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return ListSelectionViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: ListSelectionViewHolder, position: Int) {
+        holder.binding.itemNumber.text = (position + 1).toString()
+        holder.binding.itemString.text = lists[position].name
+    }
+
+    override fun getItemCount(): Int {
+        return lists.size
+    }
+
+    fun listsUpdated() {
+       notifyItemInserted(lists.size-1)
+    }
+}
