df6757297d8b46db33d090a7b14889b07c14d799
Fuad Kamal <fuad@anaara.com>
2021-03-02 23:01:54 -0500

    updates from previous chapter
    replace all the updated files from the previous chapter, that are not touched in this chapter

--- 24-podcast-subscriptions/projects/final/PodPlay/app/src/main/java/com/raywenderlich/podplay/adapter/PodcastListAdapter.kt
+++ 24-podcast-subscriptions/projects/final/PodPlay/app/src/main/java/com/raywenderlich/podplay/adapter/PodcastListAdapter.kt
@@ -1,35 +1,31 @@
 /*
- * Copyright (c) 2021 Razeware LLC
+ * Copyright (c) 2020 Razeware LLC
  *
- *  Permission is hereby granted, free of charge, to any person obtaining a copy
- *   of this software and associated documentation files (the "Software"), to deal
- *   in the Software without restriction, including without limitation the rights
- *   to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- *   copies of the Software, and to permit persons to whom the Software is
- *   furnished to do so, subject to the following conditions:
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
  *
- *  The above copyright notice and this permission notice shall be included in
- *  all copies or substantial portions of the Software.
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
  *
- *   Notwithstanding the foregoing, you may not use, copy, modify, merge, publish,
- *   distribute, sublicense, create a derivative work, and/or sell copies of the
- *   Software in any work that is designed, intended, or marketed for pedagogical or
- *   instructional purposes related to programming, coding, application development,
- *   or information technology.  Permission for such use, copying, modification,
- *   merger, publication, distribution, sublicensing, creation of derivative works,
- *   or sale is expressly withheld.
+ * Notwithstanding the foregoing, you may not use, copy, modify, merge, publish,
+ * distribute, sublicense, create a derivative work, and/or sell copies of the
+ * Software in any work that is designed, intended, or marketed for pedagogical or
+ * instructional purposes related to programming, coding, application development,
+ * or information technology.  Permission for such use, copying, modification,
+ * merger, publication, distribution, sublicensing, creation of derivative works,
+ * or sale is expressly withheld.
  *
- *   This project and source code may use libraries or frameworks that are
- *   released under various Open-Source licenses. Use of those libraries and
- *   frameworks are governed by their own individual licenses.
- *
- *   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- *   IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- *   FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- *   AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- *   LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- *   OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- *   THE SOFTWARE.
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
  */
 
 package com.raywenderlich.podplay.adapter
@@ -44,18 +40,20 @@ import com.bumptech.glide.Glide
 import com.raywenderlich.podplay.databinding.SearchItemBinding
 import com.raywenderlich.podplay.viewmodel.SearchViewModel.PodcastSummaryViewData
 
-class PodcastListAdapter(private var podcastSummaryViewList: List<PodcastSummaryViewData>?,
-                         private val podcastListAdapterListener: PodcastListAdapterListener,
-                         private val parentActivity: Activity) :
-    RecyclerView.Adapter<PodcastListAdapter.ViewHolder>() {
+class PodcastListAdapter(
+    private var podcastSummaryViewList: List<PodcastSummaryViewData>?,
+    private val podcastListAdapterListener: PodcastListAdapterListener,
+    private val parentActivity: Activity
+) : RecyclerView.Adapter<PodcastListAdapter.ViewHolder>() {
 
   interface PodcastListAdapterListener {
     fun onShowDetails(podcastSummaryViewData: PodcastSummaryViewData)
   }
 
-  inner class ViewHolder(databinding: SearchItemBinding,
-                         private val podcastListAdapterListener: PodcastListAdapterListener) :
-      RecyclerView.ViewHolder(databinding.root) {
+  inner class ViewHolder(
+      databinding: SearchItemBinding,
+      private val podcastListAdapterListener: PodcastListAdapterListener
+  ) : RecyclerView.ViewHolder(databinding.root) {
     var podcastSummaryViewData: PodcastSummaryViewData? = null
     val nameTextView: TextView = databinding.podcastNameTextView
     val lastUpdatedTextView: TextView = databinding.podcastLastUpdatedTextView
@@ -76,8 +74,10 @@ class PodcastListAdapter(private var podcastSummaryViewList: List<PodcastSummary
   }
 
   override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PodcastListAdapter.ViewHolder {
-    return ViewHolder(SearchItemBinding.inflate(LayoutInflater.from(parent.context), parent, false),
-        podcastListAdapterListener)
+    return ViewHolder(
+        SearchItemBinding.inflate(LayoutInflater.from(parent.context), parent, false),
+        podcastListAdapterListener
+    )
   }
 
   override fun onBindViewHolder(holder: ViewHolder, position: Int) {
--- 24-podcast-subscriptions/projects/final/PodPlay/app/src/main/java/com/raywenderlich/podplay/ui/PodcastDetailsFragment.kt
+++ 24-podcast-subscriptions/projects/final/PodPlay/app/src/main/java/com/raywenderlich/podplay/ui/PodcastDetailsFragment.kt
@@ -1,40 +1,35 @@
 /*
- * Copyright (c) 2021 Razeware LLC
+ * Copyright (c) 2020 Razeware LLC
  *
- *  Permission is hereby granted, free of charge, to any person obtaining a copy
- *   of this software and associated documentation files (the "Software"), to deal
- *   in the Software without restriction, including without limitation the rights
- *   to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- *   copies of the Software, and to permit persons to whom the Software is
- *   furnished to do so, subject to the following conditions:
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
  *
- *  The above copyright notice and this permission notice shall be included in
- *  all copies or substantial portions of the Software.
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
  *
- *   Notwithstanding the foregoing, you may not use, copy, modify, merge, publish,
- *   distribute, sublicense, create a derivative work, and/or sell copies of the
- *   Software in any work that is designed, intended, or marketed for pedagogical or
- *   instructional purposes related to programming, coding, application development,
- *   or information technology.  Permission for such use, copying, modification,
- *   merger, publication, distribution, sublicensing, creation of derivative works,
- *   or sale is expressly withheld.
+ * Notwithstanding the foregoing, you may not use, copy, modify, merge, publish,
+ * distribute, sublicense, create a derivative work, and/or sell copies of the
+ * Software in any work that is designed, intended, or marketed for pedagogical or
+ * instructional purposes related to programming, coding, application development,
+ * or information technology.  Permission for such use, copying, modification,
+ * merger, publication, distribution, sublicensing, creation of derivative works,
+ * or sale is expressly withheld.
  *
- *   This project and source code may use libraries or frameworks that are
- *   released under various Open-Source licenses. Use of those libraries and
- *   frameworks are governed by their own individual licenses.
- *
- *   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- *   IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- *   FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- *   AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- *   LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- *   OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- *   THE SOFTWARE.
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
  */
 
 package com.raywenderlich.podplay.ui
 
-import android.content.Context
 import android.os.Bundle
 import android.text.method.ScrollingMovementMethod
 import android.view.*
@@ -53,8 +48,6 @@ class PodcastDetailsFragment : Fragment() {
   private val podcastViewModel: PodcastViewModel by activityViewModels()
   private lateinit var databinding: FragmentPodcastDetailsBinding
   private lateinit var episodeListAdapter: EpisodeListAdapter
-  private var listener: OnPodcastDetailsListener? = null
-  private var menuItem: MenuItem? = null
 
   companion object {
     fun newInstance(): PodcastDetailsFragment {
@@ -73,81 +66,37 @@ class PodcastDetailsFragment : Fragment() {
     return databinding.root
   }
 
-  override fun onActivityCreated(savedInstanceState: Bundle?) {
-    super.onActivityCreated(savedInstanceState)
-    setupControls()
-    updateControls()
+  override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+    super.onViewCreated(view, savedInstanceState)
+
+    podcastViewModel.podcastLiveData.observe(viewLifecycleOwner, { viewData ->
+      if (viewData != null) {
+        databinding.feedTitleTextView.text = viewData.feedTitle
+        databinding.feedDescTextView.text = viewData.feedDesc
+        activity?.let { activity ->
+          Glide.with(activity).load(viewData.imageUrl).into(databinding.feedImageView)
+        }
+
+        // 1
+        databinding.feedDescTextView.movementMethod = ScrollingMovementMethod()
+        // 2
+        databinding.episodeRecyclerView.setHasFixedSize(true)
+
+        val layoutManager = LinearLayoutManager(activity)
+        databinding.episodeRecyclerView.layoutManager = layoutManager
+
+        val dividerItemDecoration = DividerItemDecoration(
+            databinding.episodeRecyclerView.context, layoutManager.orientation)
+        databinding.episodeRecyclerView.addItemDecoration(dividerItemDecoration)
+        // 3
+        episodeListAdapter = EpisodeListAdapter(viewData.episodes)
+        databinding.episodeRecyclerView.adapter = episodeListAdapter
+      }
+    })
   }
 
   override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
     super.onCreateOptionsMenu(menu, inflater)
     inflater.inflate(R.menu.menu_details, menu)
-    menuItem = menu.findItem(R.id.menu_feed_action)
-    updateMenuItem()
-  }
-
-  override fun onAttach(context: Context) {
-    super.onAttach(context)
-    if (context is OnPodcastDetailsListener) {
-      listener = context
-    } else {
-      throw RuntimeException(context.toString() + " must implement OnPodcastDetailsListener")
-    }
-  }
-
-  override fun onOptionsItemSelected(item: MenuItem): Boolean {
-    when (item.itemId) {
-      R.id.menu_feed_action -> {
-        podcastViewModel.activePodcastViewData?.feedUrl?.let {
-
-          if (podcastViewModel.activePodcastViewData?.subscribed == true) {
-            listener?.onUnsubscribe()
-          } else {
-            listener?.onSubscribe()
-          }
-        }
-        return true
-      }
-      else ->
-        return super.onOptionsItemSelected(item)
-    }
-  }
-
-  private fun updateControls() {
-    val viewData = podcastViewModel.activePodcastViewData ?: return
-    databinding.feedTitleTextView.text = viewData.feedTitle
-    databinding.feedDescTextView.text = viewData.feedDesc
-    activity?.let { activity ->
-      Glide.with(activity).load(viewData.imageUrl).into(databinding.feedImageView)
-    }
-  }
-
-  private fun setupControls() {
-    // 1
-    databinding.feedDescTextView.movementMethod = ScrollingMovementMethod()
-    // 2
-    databinding.episodeRecyclerView.setHasFixedSize(true)
-
-    val layoutManager = LinearLayoutManager(activity)
-    databinding.episodeRecyclerView.layoutManager = layoutManager
-
-    val dividerItemDecoration = DividerItemDecoration(
-        databinding.episodeRecyclerView.context, layoutManager.orientation)
-    databinding.episodeRecyclerView.addItemDecoration(dividerItemDecoration)
-    // 3
-    episodeListAdapter = EpisodeListAdapter(
-        podcastViewModel.activePodcastViewData?.episodes)
-    databinding.episodeRecyclerView.adapter = episodeListAdapter
-  }
-
-  private fun updateMenuItem() {
-    val viewData = podcastViewModel.activePodcastViewData ?: return
-    menuItem?.title = if (viewData.subscribed) getString(R.string.unsubscribe)
-    else getString(R.string.subscribe)
-  }
-
-  interface OnPodcastDetailsListener {
-    fun onSubscribe()
-    fun onUnsubscribe()
   }
 }
