d372ea66d570cd0e505e9bc2bd01451fad2bc7d9
Fuad Kamal <fuad@anaara.com>
2021-03-06 21:30:02 -0500

    reset final project
    replaced final project w/starter project, working through chapter to update

--- 26-podcast-playback/projects/final/PodPlay/app/src/main/java/com/raywenderlich/podplay/service/RssFeedService.kt
+++ 26-podcast-playback/projects/final/PodPlay/app/src/main/java/com/raywenderlich/podplay/service/RssFeedService.kt
@@ -1,84 +1,116 @@
 /*
- * Copyright (c) 2020 Razeware LLC
+ * Copyright (c) 2021 Razeware LLC
  *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
+ *  Permission is hereby granted, free of charge, to any person obtaining a copy
+ *   of this software and associated documentation files (the "Software"), to deal
+ *   in the Software without restriction, including without limitation the rights
+ *   to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *   copies of the Software, and to permit persons to whom the Software is
+ *   furnished to do so, subject to the following conditions:
  *
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
+ *  The above copyright notice and this permission notice shall be included in
+ *  all copies or substantial portions of the Software.
  *
- * Notwithstanding the foregoing, you may not use, copy, modify, merge, publish,
- * distribute, sublicense, create a derivative work, and/or sell copies of the
- * Software in any work that is designed, intended, or marketed for pedagogical or
- * instructional purposes related to programming, coding, application development,
- * or information technology.  Permission for such use, copying, modification,
- * merger, publication, distribution, sublicensing, creation of derivative works,
- * or sale is expressly withheld.
+ *   Notwithstanding the foregoing, you may not use, copy, modify, merge, publish,
+ *   distribute, sublicense, create a derivative work, and/or sell copies of the
+ *   Software in any work that is designed, intended, or marketed for pedagogical or
+ *   instructional purposes related to programming, coding, application development,
+ *   or information technology.  Permission for such use, copying, modification,
+ *   merger, publication, distribution, sublicensing, creation of derivative works,
+ *   or sale is expressly withheld.
  *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- * THE SOFTWARE.
+ *   This project and source code may use libraries or frameworks that are
+ *   released under various Open-Source licenses. Use of those libraries and
+ *   frameworks are governed by their own individual licenses.
+ *
+ *   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *   IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *   FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *   AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *   LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *   OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ *   THE SOFTWARE.
  */
 
 package com.raywenderlich.podplay.service
 
+import com.raywenderlich.podplay.BuildConfig
 import com.raywenderlich.podplay.util.DateUtils
-import okhttp3.*
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+import okhttp3.OkHttpClient
+import okhttp3.ResponseBody
+import okhttp3.logging.HttpLoggingInterceptor
 import org.w3c.dom.Node
-import java.io.IOException
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.http.GET
+import retrofit2.http.Headers
+import retrofit2.http.Url
+import java.util.concurrent.TimeUnit
 import javax.xml.parsers.DocumentBuilderFactory
 
-class RssFeedService: FeedService  {
-  override fun getFeed(xmlFileURL: String, callBack: (RssFeedResponse?) -> Unit) {
+class RssFeedService private constructor() {
 
-    val client = OkHttpClient()
+  suspend fun getFeed(xmlFileURL: String): RssFeedResponse? {
+    val service: FeedService
 
-    val request = Request.Builder()
-        .url(xmlFileURL)
+    val interceptor = HttpLoggingInterceptor()
+    interceptor.level = HttpLoggingInterceptor.Level.BODY
+
+    val client = OkHttpClient().newBuilder()
+        .connectTimeout(30, TimeUnit.SECONDS)
+        .writeTimeout(30, TimeUnit.SECONDS)
+        .readTimeout(30, TimeUnit.SECONDS)
+
+    if (BuildConfig.DEBUG) {
+      client.addInterceptor(interceptor)
+    }
+    client.build()
+
+    val retrofit = Retrofit.Builder()
+        .baseUrl("${xmlFileURL.split("?")[0]}/")
         .build()
+    service = retrofit.create(FeedService::class.java)
 
-    client.newCall(request).enqueue(object : Callback {
-      override fun onFailure(call: Call, e: IOException) {
-        callBack(null)
-      }
-
-      @Throws(IOException::class)
-      override fun onResponse(call: Call, response: Response) {
-        if (response.isSuccessful) {
-          response.body()?.let { responseBody ->
-            val dbFactory = DocumentBuilderFactory.newInstance()
-            val dBuilder = dbFactory.newDocumentBuilder()
-            val doc = dBuilder.parse(responseBody.byteStream())
-            val rssFeedResponse = RssFeedResponse(episodes = mutableListOf())
-            domToRssFeedResponse(doc, rssFeedResponse)
-            callBack(rssFeedResponse)
-            return
-          }
+    try {
+      val result = service.getFeed(xmlFileURL)
+      if (result.code() >= 400) {
+        // TODO : // create an error from error body and return
+        println("server error, ${result.code()}, ${result.errorBody()}")
+        return null
+      } else {
+        var rssFeedResponse: RssFeedResponse?
+        val dbFactory = DocumentBuilderFactory.newInstance()
+        val dBuilder = dbFactory.newDocumentBuilder()
+        withContext(Dispatchers.IO) {
+          val doc = dBuilder.parse(result.body()?.byteStream())
+          val rss = RssFeedResponse(episodes = mutableListOf())
+          domToRssFeedResponse(doc, rss)
+          println(rss)
+          rssFeedResponse = rss
         }
-        callBack(null)
+        return rssFeedResponse
       }
-    })
+    } catch (t: Throwable) {
+      // TODO : create an error from throwable and return it
+      println("error, ${t.localizedMessage}")
+    }
+    return null
   }
 
   private fun domToRssFeedResponse(node: Node, rssFeedResponse: RssFeedResponse) {
-
     if (node.nodeType == Node.ELEMENT_NODE) {
-
       val nodeName = node.nodeName
       val parentName = node.parentNode.nodeName
+      // 1
       val grandParentName = node.parentNode.parentNode?.nodeName ?: ""
-
+      // 2
       if (parentName == "item" && grandParentName == "channel") {
+        // 3
         val currentItem = rssFeedResponse.episodes?.last()
         if (currentItem != null) {
+          // 4
           when (nodeName) {
             "title" -> currentItem.title = node.textContent
             "description" -> currentItem.description = node.textContent
@@ -87,39 +119,43 @@ class RssFeedService: FeedService  {
             "pubDate" -> currentItem.pubDate = node.textContent
             "link" -> currentItem.link = node.textContent
             "enclosure" -> {
-              currentItem.url = node.attributes.getNamedItem("url").textContent
-              currentItem.type = node.attributes.getNamedItem("type").textContent
+              currentItem.url = node.attributes.getNamedItem("url")
+                  .textContent
+              currentItem.type = node.attributes.getNamedItem("type")
+                  .textContent
             }
           }
         }
       }
-
       if (parentName == "channel") {
         when (nodeName) {
           "title" -> rssFeedResponse.title = node.textContent
           "description" -> rssFeedResponse.description = node.textContent
           "itunes:summary" -> rssFeedResponse.summary = node.textContent
           "item" -> rssFeedResponse.episodes?.add(RssFeedResponse.EpisodeResponse())
-          "pubDate" -> rssFeedResponse.lastUpdated = DateUtils.xmlDateToDate(node.textContent)
+          "pubDate" -> rssFeedResponse.lastUpdated =
+              DateUtils.xmlDateToDate(node.textContent)
         }
       }
     }
-
     val nodeList = node.childNodes
-
     for (i in 0 until nodeList.length) {
       val childNode = nodeList.item(i)
       domToRssFeedResponse(childNode, rssFeedResponse)
     }
   }
-}
-
-interface FeedService {
-  fun getFeed(xmlFileURL: String, callBack: (RssFeedResponse?) -> Unit)
-
   companion object {
-    val instance: FeedService by lazy {
+    val instance: RssFeedService by lazy {
       RssFeedService()
     }
   }
+}
+
+interface FeedService {
+  @Headers(
+      "Content-Type: application/xml; charset=utf-8",
+      "Accept: application/xml"
+  )
+  @GET
+  suspend fun getFeed(@Url xmlFileURL: String): Response<ResponseBody>
 }
--- 26-podcast-playback/projects/final/PodPlay/app/src/main/java/com/raywenderlich/podplay/viewmodel/SearchViewModel.kt
+++ 26-podcast-playback/projects/final/PodPlay/app/src/main/java/com/raywenderlich/podplay/viewmodel/SearchViewModel.kt
@@ -1,31 +1,35 @@
 /*
- * Copyright (c) 2020 Razeware LLC
+ * Copyright (c) 2021 Razeware LLC
  *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
+ *  Permission is hereby granted, free of charge, to any person obtaining a copy
+ *   of this software and associated documentation files (the "Software"), to deal
+ *   in the Software without restriction, including without limitation the rights
+ *   to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *   copies of the Software, and to permit persons to whom the Software is
+ *   furnished to do so, subject to the following conditions:
  *
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
+ *  The above copyright notice and this permission notice shall be included in
+ *  all copies or substantial portions of the Software.
  *
- * Notwithstanding the foregoing, you may not use, copy, modify, merge, publish,
- * distribute, sublicense, create a derivative work, and/or sell copies of the
- * Software in any work that is designed, intended, or marketed for pedagogical or
- * instructional purposes related to programming, coding, application development,
- * or information technology.  Permission for such use, copying, modification,
- * merger, publication, distribution, sublicensing, creation of derivative works,
- * or sale is expressly withheld.
+ *   Notwithstanding the foregoing, you may not use, copy, modify, merge, publish,
+ *   distribute, sublicense, create a derivative work, and/or sell copies of the
+ *   Software in any work that is designed, intended, or marketed for pedagogical or
+ *   instructional purposes related to programming, coding, application development,
+ *   or information technology.  Permission for such use, copying, modification,
+ *   merger, publication, distribution, sublicensing, creation of derivative works,
+ *   or sale is expressly withheld.
  *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- * THE SOFTWARE.
+ *   This project and source code may use libraries or frameworks that are
+ *   released under various Open-Source licenses. Use of those libraries and
+ *   frameworks are governed by their own individual licenses.
+ *
+ *   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *   IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *   FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *   AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *   LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *   OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ *   THE SOFTWARE.
  */
 
 package com.raywenderlich.podplay.viewmodel
@@ -45,27 +49,28 @@ class SearchViewModel(application: Application) : AndroidViewModel(application) 
       var lastUpdated: String? = "",
       var imageUrl: String? = "",
       var feedUrl: String? = "")
-  
+
   private fun itunesPodcastToPodcastSummaryView(
       itunesPodcast: PodcastResponse.ItunesPodcast):
       PodcastSummaryViewData {
     return PodcastSummaryViewData(
         itunesPodcast.collectionCensoredName,
         DateUtils.jsonDateToShortDate(itunesPodcast.releaseDate),
-        itunesPodcast.artworkUrl100,
+        itunesPodcast.artworkUrl30,
         itunesPodcast.feedUrl)
   }
 
-  fun searchPodcasts(term: String, callback: (List<PodcastSummaryViewData>) -> Unit) {
-    iTunesRepo?.searchByTerm(term) { results ->
-      if (results == null) {
-        callback(emptyList())
-      } else {
-        val searchViews = results.map { podcast ->
+  suspend fun searchPodcasts(term: String): List<PodcastSummaryViewData> {
+    val results = iTunesRepo?.searchByTerm(term)
+
+    if (results != null && results.isSuccessful) {
+      val podcasts = results.body()?.results
+      if (!podcasts.isNullOrEmpty()) {
+        return podcasts.map { podcast ->
           itunesPodcastToPodcastSummaryView(podcast)
         }
-        callback(searchViews)
       }
     }
+    return emptyList()
   }
 }
