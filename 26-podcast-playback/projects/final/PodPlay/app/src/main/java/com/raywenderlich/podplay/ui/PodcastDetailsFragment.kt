f429da26517790a36d64974352b80213f6204ec3
Fuad Kamal <fuad@anaara.com>
2021-03-08 16:31:35 -0500

    update for LiveData, View binding, etc.
    update to accomodate changes in prior chapters

--- 26-podcast-playback/projects/final/PodPlay/app/src/main/java/com/raywenderlich/podplay/ui/PodcastDetailsFragment.kt
+++ 26-podcast-playback/projects/final/PodPlay/app/src/main/java/com/raywenderlich/podplay/ui/PodcastDetailsFragment.kt
@@ -34,11 +34,19 @@
 
 package com.raywenderlich.podplay.ui
 
+import android.content.ComponentName
 import android.content.Context
+import android.net.Uri
 import android.os.Bundle
+import android.support.v4.media.MediaBrowserCompat
+import android.support.v4.media.MediaMetadataCompat
+import android.support.v4.media.session.MediaControllerCompat
+import android.support.v4.media.session.MediaSessionCompat
+import android.support.v4.media.session.PlaybackStateCompat
 import android.text.method.ScrollingMovementMethod
 import android.view.*
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
 import androidx.fragment.app.activityViewModels
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -46,6 +54,7 @@ import com.bumptech.glide.Glide
 import com.raywenderlich.podplay.R
 import com.raywenderlich.podplay.adapter.EpisodeListAdapter
 import com.raywenderlich.podplay.databinding.FragmentPodcastDetailsBinding
+import com.raywenderlich.podplay.service.PodplayMediaService
 import com.raywenderlich.podplay.viewmodel.PodcastViewModel
 
 class PodcastDetailsFragment : Fragment() {
@@ -54,6 +63,9 @@ class PodcastDetailsFragment : Fragment() {
   private lateinit var databinding: FragmentPodcastDetailsBinding
   private lateinit var episodeListAdapter: EpisodeListAdapter
   private var listener: OnPodcastDetailsListener? = null
+  private var menuItem: MenuItem? = null
+  private lateinit var mediaBrowser: MediaBrowserCompat
+  private var mediaControllerCallback: MediaControllerCallback? = null
 
   companion object {
     fun newInstance(): PodcastDetailsFragment {
@@ -64,6 +76,7 @@ class PodcastDetailsFragment : Fragment() {
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setHasOptionsMenu(true)
+    initMediaBrowser()
   }
 
   override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
@@ -126,6 +139,116 @@ class PodcastDetailsFragment : Fragment() {
       }
       else ->
         super.onOptionsItemSelected(item)
+    }
+  }
+
+  override fun onStart() {
+    super.onStart()
+    if (mediaBrowser.isConnected) {
+      val fragmentActivity = activity as FragmentActivity
+      if (MediaControllerCompat.getMediaController(fragmentActivity) == null) {
+        registerMediaController(mediaBrowser.sessionToken)
+      }
+    } else {
+      mediaBrowser.connect()
+    }
+  }
+
+  override fun onStop() {
+    super.onStop()
+    val fragmentActivity = activity as FragmentActivity
+    if (MediaControllerCompat.getMediaController(fragmentActivity) != null) {
+      mediaControllerCallback?.let {
+        MediaControllerCompat.getMediaController(fragmentActivity)
+            .unregisterCallback(it)
+      }
+    }
+  }
+
+  private fun setupControls() {
+    databinding.feedDescTextView.movementMethod = ScrollingMovementMethod()
+    databinding.episodeRecyclerView.setHasFixedSize(true)
+    val layoutManager = LinearLayoutManager(activity)
+    databinding.episodeRecyclerView.layoutManager = layoutManager
+    val dividerItemDecoration = DividerItemDecoration(
+        databinding.episodeRecyclerView.context, layoutManager.orientation)
+    databinding.episodeRecyclerView.addItemDecoration(dividerItemDecoration)
+
+    episodeListAdapter = EpisodeListAdapter(podcastViewModel.podcastLiveData.value?.episodes)
+    databinding.episodeRecyclerView.adapter = episodeListAdapter
+  }
+
+  private fun updateControls() {
+    val viewData = podcastViewModel.podcastLiveData
+    databinding.feedTitleTextView.text = viewData.value?.feedTitle
+    databinding.feedDescTextView.text = viewData.value?.feedDesc
+    activity?.let { activity ->
+      Glide.with(activity).load(viewData.value?.imageUrl).into(databinding.feedImageView)
+    }
+  }
+
+  private fun updateMenuItem() {
+    val viewData = podcastViewModel.podcastLiveData
+    menuItem?.title = if (viewData.value?.subscribed ?: false) getString(R.string.unsubscribe)
+    else getString(R.string.subscribe)
+  }
+
+  private fun registerMediaController(token: MediaSessionCompat.Token) {
+    val fragmentActivity = activity as FragmentActivity
+    val mediaController = MediaControllerCompat(fragmentActivity, token)
+    MediaControllerCompat.setMediaController(fragmentActivity, mediaController)
+    mediaControllerCallback = MediaControllerCallback()
+    mediaController.registerCallback(mediaControllerCallback!!)
+  }
+
+  private fun initMediaBrowser() {
+    val fragmentActivity = activity as FragmentActivity
+    mediaBrowser = MediaBrowserCompat(fragmentActivity,
+        ComponentName(fragmentActivity, PodplayMediaService::class.java),
+        MediaBrowserCallBacks(),
+        null)
+  }
+
+  private fun startPlaying(episodeViewData: PodcastViewModel.EpisodeViewData) {
+    val fragmentActivity = activity as FragmentActivity
+    val controller = MediaControllerCompat.getMediaController(fragmentActivity)
+    val viewData = podcastViewModel.podcastLiveData
+    val bundle = Bundle()
+
+    bundle.putString(MediaMetadataCompat.METADATA_KEY_TITLE, episodeViewData.title)
+    bundle.putString(MediaMetadataCompat.METADATA_KEY_ARTIST, viewData.value?.feedTitle)
+    bundle.putString(MediaMetadataCompat.METADATA_KEY_ALBUM_ART_URI, viewData.value?.imageUrl)
+
+    controller.transportControls.playFromUri(Uri.parse(episodeViewData.mediaUrl), bundle)
+  }
+
+  inner class MediaBrowserCallBacks: MediaBrowserCompat.ConnectionCallback() {
+
+    override fun onConnected() {
+      super.onConnected()
+      registerMediaController(mediaBrowser.sessionToken)
+      println("onConnected")
+    }
+
+    override fun onConnectionSuspended() {
+      super.onConnectionSuspended()
+      println("onConnectionSuspended")
+      // Disable transport controls
+    }
+
+    override fun onConnectionFailed() {
+      super.onConnectionFailed()
+      println("onConnectionFailed")
+      // Fatal error handling
+    }
+  }
+
+  inner class MediaControllerCallback: MediaControllerCompat.Callback() {
+    override fun onMetadataChanged(metadata: MediaMetadataCompat?) {
+      println("metadata changed to ${metadata?.getString(MediaMetadataCompat.METADATA_KEY_MEDIA_URI)}")
+    }
+    override fun onPlaybackStateChanged(state: PlaybackStateCompat?) {
+      println("state changed to $state")
     }
   }
 
