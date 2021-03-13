0f4b521d1e946b695409469a3fc065d9ac198987
Kevin Moore <kevindavmoore@gmail.com>
2021-03-12 21:38:07 -0800

    Chapter 8 TE Update

--- 08-shared-preferences/projects/final/ListMaker/app/src/main/java/com/raywenderlich/listmaker/ui/main/ListSelectionViewHolder.kt
+++ 08-shared-preferences/projects/final/ListMaker/app/src/main/java/com/raywenderlich/listmaker/ui/main/ListSelectionViewHolder.kt
@@ -1,6 +1,5 @@
 package com.raywenderlich.listmaker.ui.main
 
-import android.view.View
 import androidx.recyclerview.widget.RecyclerView
 import com.raywenderlich.listmaker.databinding.ListSelectionViewHolderBinding
 
