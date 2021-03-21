7c747b4f145d67d69492f7f93e7935f4c5a86214
Ellen Shapiro <designatednerd@gmail.com>
2021-03-21 15:30:53 -0500

    update starter based on ch1 final

--- 02-layouts/projects/starter/timefighter/app/src/main/java/com/raywenderlich/timefighter/MainActivity.kt
+++ 02-layouts/projects/starter/timefighter/app/src/main/java/com/raywenderlich/timefighter/MainActivity.kt
@@ -1,12 +1,11 @@
 package com.raywenderlich.timefighter
 
-import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-    }
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_main)
+  }
 }
